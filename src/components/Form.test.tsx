c73192d4eb2b1d18b74366cc11d39a5f6374030f
Leticia Santanna <leticiafsn_@hotmail.com>
2023-01-12 15:07:11 -0300

    created app and write first test

---
+++ src/components/Form.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import Form from "./Form";
+
+test("Quando input está vazio, não podem ser incluidos novos participantes", () => {
+  render(<Form />);
+
+  const input = screen.getByPlaceholderText(
+    "Adicione os nomes dos participantes"
+  );
+  const button = screen.getByRole("button");
+
+  expect(input).toBeInTheDocument();
+  expect(button).toBeDisabled();
+});
---
+++ src/components/Form.tsx
@@ -0,0 +1,12 @@
+const Form = () => {
+  return (
+    <>
+      <form>
+        <input type="text" placeholder="Adicione os nomes dos participantes" />
+        <button disabled>Adicionar</button>
+      </form>
+    </>
+  );
+};
+
+export default Form;
